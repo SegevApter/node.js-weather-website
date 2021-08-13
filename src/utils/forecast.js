d6e82ca483e76389526e7d51b95dd28032f5587c
SegevApter <segevapter@gmail.com>
2021-08-13 18:27:53 -0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,16 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f5ca50268ffd0b8c07cd4099a2f2f9c1&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({url, json: true}, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. The temprature is ' + body.current.temperature + ' but it feels like ' + body.current.feelslike + ' degrees outside! :) ')
+        }
+    })
+}
+module.exports = forecast
